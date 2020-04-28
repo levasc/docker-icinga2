96cfd25719450dd419c83c18e978cd0fa63c8ba7
Levas Cernovas <levas.cernovas@gmail.com>
2020-04-28 11:07:04 +0300

    added ipl

--- install.sh
+++ install.sh
@@ -141,19 +141,12 @@ EOF
 
 #Module installation - Graphite, Director
 #Director
-# mkdir -p /usr/share/icingaweb2/modules/
+#mkdir -p /usr/share/icingaweb2/modules/
 # mkdir -p /usr/share/icingaweb2/modules/director/
 # wget -q --no-cookies -O - https://github.com/Icinga/icingaweb2-module-director/archive/v1.4.3.tar.gz | tar xz --strip-components=1 --directory=/usr/share/icingaweb2/modules/director --exclude=.gitignore -f -
 # ln -s /usr/share/icingaweb2/modules/director/ /etc/icingaweb2/enabledModules/director
 
-#installing ipl module
 
-MODULE_NAME=ipl
-MODULE_VERSION=v0.5.0
-REPO="https://github.com/Icinga/icingaweb2-module-${MODULE_NAME}"
-MODULES_PATH="/usr/share/icingaweb2/modules"
-git clone ${REPO} "${MODULES_PATH}/${MODULE_NAME}" --branch "${MODULE_VERSION}"
-icingacli module enable "${MODULE_NAME}"
 
 
 #Graphite
